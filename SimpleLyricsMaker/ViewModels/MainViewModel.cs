bb1ff0a8f12dc2b5c49b2509f3b356449e75b848
李继龙 <kljzndx@outlook.com>
2019-11-07 09:29:46 +0800

    完成mvvm框架搭建

---
+++ SimpleLyricsMaker/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace SimpleLyricsMaker.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        
+    }
+}
---
+++ SimpleLyricsMaker/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Design;
+using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace SimpleLyricsMaker.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+    }
+}

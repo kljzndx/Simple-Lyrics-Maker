89a07b620dbbd0177884727fd94b40bb103d267b
李继龙 <kljzndx@outlook.com>
2019-11-29 18:58:15 +0800

    新增音乐文件模型

---
+++ SimpleLyricsMaker/Models/MusicFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+
+namespace SimpleLyricsMaker.Models
+{
+    public class MusicFile
+    {
+        private MusicFile(StorageFile file, StorageItemThumbnail albumCover, MusicProperties musicProperties)
+        {
+            File = file;
+            AlbumCover = albumCover;
+
+            Title = musicProperties.Title;
+            Artist = musicProperties.Artist;
+            Album = musicProperties.Album;
+        }
+
+        public string Title { get; }
+        public string Artist { get; }
+        public string Album { get; }
+        public StorageItemThumbnail AlbumCover { get; }
+        public StorageFile File { get; }
+
+        public static async Task<MusicFile> Create(StorageFile file)
+        {
+            return new MusicFile(file, await file.GetThumbnailAsync(ThumbnailMode.MusicView), await file.Properties.GetMusicPropertiesAsync());
+        }
+    }
+}

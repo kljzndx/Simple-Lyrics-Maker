cc5c6ec1a314e00db7c49ea5277d7ee132a3b32c
李继龙 <kljzndx@outlook.com>
2019-12-02 10:45:00 +0800

    完善音乐文件模型

--- SimpleLyricsMaker/Models/MusicFile.cs
+++ SimpleLyricsMaker/Models/MusicFile.cs
@@ -2,30 +2,62 @@
 using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.Storage.FileProperties;
+using Windows.UI.Xaml.Media.Imaging;
+using SimpleLyricsMaker.ViewModels.Extensions;
 
 namespace SimpleLyricsMaker.Models
 {
     public class MusicFile
     {
-        private MusicFile(StorageFile file, StorageItemThumbnail albumCover, MusicProperties musicProperties)
-        {
-            File = file;
-            AlbumCover = albumCover;
+        private readonly WeakReference<StorageFile> _file = new WeakReference<StorageFile>(null);
+        private readonly WeakReference<StorageItemThumbnail> _albumCoverSource = new WeakReference<StorageItemThumbnail>(null);
+        private readonly WeakReference<BitmapImage> _albumCover = new WeakReference<BitmapImage>(null);
 
+        private MusicFile(StorageFile file, MusicProperties musicProperties)
+        {
             Title = musicProperties.Title;
             Artist = musicProperties.Artist;
             Album = musicProperties.Album;
+
+            FileName = file.Name;
+            FilePath = file.Path;
         }
 
         public string Title { get; }
         public string Artist { get; }
         public string Album { get; }
-        public StorageItemThumbnail AlbumCover { get; }
-        public StorageFile File { get; }
+
+        public string FileName { get; }
+        public string FilePath { get; }
+
+        public async Task<StorageFile> GetFile()
+        {
+            return await _file.GetTarget(async () => await StorageFile.GetFileFromPathAsync(FilePath));
+        }
+
+        public async Task<StorageItemThumbnail> GetAlbumCoverSource()
+        {
+            return await _albumCoverSource.GetTarget(async () =>
+            {
+                var file = await GetFile();
+                return await file.GetThumbnailAsync(ThumbnailMode.MusicView);
+            });
+        }
+
+        public async Task<BitmapImage> GetAlbumCover()
+        {
+            return await _albumCover.GetTarget(async () =>
+            {
+                var cover = await GetAlbumCoverSource();
+                var image = new BitmapImage();
+                image.SetSource(cover);
+                return image;
+            });
+        }
 
         public static async Task<MusicFile> Create(StorageFile file)
         {
-            return new MusicFile(file, await file.GetThumbnailAsync(ThumbnailMode.MusicView), await file.Properties.GetMusicPropertiesAsync());
+            return new MusicFile(file, await file.Properties.GetMusicPropertiesAsync());
         }
     }
 }
---
+++ SimpleLyricsMaker/ViewModels/Extensions/WeakReferenceExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SimpleLyricsMaker.ViewModels.Extensions
+{
+    public static class WeakReferenceExtension
+    {
+        public static async Task<T> GetTarget<T>(this WeakReference<T> wr, Func<Task<T>> objectFactory) where T : class
+        {
+            T obj = default(T);
+            if (wr.TryGetTarget(out obj))
+                return obj;
+            else
+            {
+                obj = await objectFactory();
+                wr.SetTarget(obj);
+                return obj;
+            }
+        }
+    }
+}

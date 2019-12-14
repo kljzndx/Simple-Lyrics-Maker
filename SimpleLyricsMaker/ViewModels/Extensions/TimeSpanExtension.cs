3f3b8aa580fb21afb2d3c5cc51c04339d23c3673
李继龙 <kljzndx@outlook.com>
2019-12-14 16:15:49 +0800

    Fixed bugs

--- SimpleLyricsMaker/ViewModels/Extensions/TimeSpanExtension.cs
+++ SimpleLyricsMaker/ViewModels/Extensions/TimeSpanExtension.cs
@@ -9,7 +9,7 @@ namespace SimpleLyricsMaker.ViewModels.Extensions
             string result = "";
 
             int hours = time.Hours + (24 * time.Days);
-            if (time.Hours > 0)
+            if (hours > 0)
                 result += $"{hours:D2}:";
             
             return result + $"{time.Minutes:D2}:{time.Seconds:D2}.{time.Milliseconds.ToString("D3").Substring(0, 2)}";

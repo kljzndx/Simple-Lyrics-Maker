5196f7a39fe687f7f73be7a39d0a48fd0a0a2241
李继龙 <kljzndx@outlook.com>
2019-12-18 21:33:47 +0800

    优化内容显示

---
+++ SimpleLyricsMaker/ViewModels/ValueConverters/BilingualStringToNormalString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml.Data;
+using SimpleLyricsMaker.ViewModels.Settings;
+
+namespace SimpleLyricsMaker.ViewModels.ValueConverters
+{
+    public class BilingualStringToNormalString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string str = (string) value;
+            var lines = str.Split(EditViewSettingProperties.Current.SplitSymbol).Select(s => s.Trim()).ToList();
+            if (lines.Count == 1)
+                return str;
+            return String.Join(Environment.NewLine, lines);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

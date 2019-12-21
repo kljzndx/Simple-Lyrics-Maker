ea533e4ac8937375539f03386b9f7b51366d362c
李继龙 <kljzndx@outlook.com>
2019-12-21 16:14:50 +0800

    砍掉一些没用的玩意

--- SimpleLyricsMaker/ViewModels/MainViewModel.cs
+++ SimpleLyricsMaker/ViewModels/MainViewModel.cs
@@ -7,16 +7,5 @@ namespace SimpleLyricsMaker.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        public MainViewModel()
-        {
-            PagesList = new ObservableCollection<PageModel>
-            {
-                new PageModel('\uE142', "信息编辑", typeof(EditPage)),
-                new PageModel('\uE15E', "字幕打点", null),
-                new PageModel('\uE052', "完成预览", null),
-            };
-        }
-
-        public ObservableCollection<PageModel> PagesList { get; }
     }
 }
--- SimpleLyricsMaker/Views/UIFramework.xaml.cs
+++ SimpleLyricsMaker/Views/UIFramework.xaml.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using SimpleLyricsMaker.Logs;
-using SimpleLyricsMaker.Models;
-using SimpleLyricsMaker.ViewModels;
+﻿using Windows.UI.Xaml.Controls;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -16,27 +9,9 @@ namespace SimpleLyricsMaker.Views
     /// </summary>
     public sealed partial class UIFramework : Page
     {
-        private MainViewModel _vm;
-
         public UIFramework()
         {
             this.InitializeComponent();
-            _vm = (MainViewModel) this.DataContext;
-        }
-
-        private void OpenPane_Button_OnClick(object sender, RoutedEventArgs e)
-        {
-            Root_SplitView.IsPaneOpen = !Root_SplitView.IsPaneOpen;
-        }
-
-        private void HamburgMenu_ListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            PageModel pm = e.AddedItems.FirstOrDefault() as PageModel;
-            if (pm?.PageType != null)
-            {
-                this.LogByObject("切换页面");
-                Main_Frame.Navigate(pm.PageType);
-            }
         }
     }
 }
